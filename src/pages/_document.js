72f70d2f334b6f1ac8936be01245fd9bea77b936
HuanPT <lexus.hc293@gmail.com>
2023-06-30 10:54:07 +0700

    huan update meta tag 30/06

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charset="UTF-8" />
+        <link rel="icon" type="image/svg+xml" href="/public/faviconLogo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          property="og:url"
+          content="https://the-movies-reactjs.vercel.app"
+        />
+        <meta property="og:type" content="movie" />
+        <meta property="og:title" content="TheMovies" />
+        <meta
+          property="og:description"
+          content="TheMovies - Mang cả thế giới giải trí đến với bạn!"
+        />
+        <meta property="og:image" content="width:600px height:314px" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
